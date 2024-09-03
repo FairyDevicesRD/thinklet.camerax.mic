ee321db2cad6dba9c4914b98ba8eb5cb14577652
Tamaki Hidetsugu <git@ralph.ms>
2024-09-04 02:08:08 +0900

    Add own repo definition

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,21 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "GitHub Packages"
+            setUrl("https://maven.pkg.github.com/FairyDevicesRD/thinklet.app.sdk")
+            credentials {
+                val properties = java.util.Properties()
+                properties.load(file("github.properties").inputStream())
+                username = properties.getProperty("username") ?: ""
+                password = properties.getProperty("token") ?: ""
+            }
+        }
+    }
+    versionCatalogs {
+        create("thinkletLibs") {
+            from(files("gradle/thinklet.versions.toml"))
+        }
     }
 }
 
