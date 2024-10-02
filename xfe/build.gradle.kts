1501b9638faf873a6a181f6d15a6640baea87c59
Maehata Kenta <maehata@fairydevices.jp>
2024-10-02 18:08:05 +0900

    Implemented internal of XfeThinkletMic
    - Added ThinkletXfe of AppSDK.

--- xfe/build.gradle.kts
+++ xfe/build.gradle.kts
@@ -36,5 +36,6 @@ dependencies {
     implementation(project(":core"))
     implementation(libs.androidx.annotation)
     implementation(thinkletLibs.camerax.camera.video)
+    implementation(thinkletLibs.sdk.audio)
     testImplementation(libs.junit)
 }
--- xfe/src/main/java/ai/fd/thinklet/camerax/mic/xfe/XfeThinkletMic.kt
+++ xfe/src/main/java/ai/fd/thinklet/camerax/mic/xfe/XfeThinkletMic.kt
@@ -4,21 +4,67 @@ import ai.fd.thinklet.camerax.ThinkletAudioRecordWrapperFactory
 import ai.fd.thinklet.camerax.ThinkletAudioSettingsPatcher
 import ai.fd.thinklet.camerax.ThinkletMic
 import ai.fd.thinklet.camerax.mic.ThinkletMics
+import ai.fd.thinklet.sdk.audio.ExperimentalXfeFeature
+import ai.fd.thinklet.sdk.audio.configureXfe
+import android.annotation.SuppressLint
+import android.media.AudioFormat
+import android.media.AudioManager
+import android.media.AudioRecord
+import androidx.camera.core.Logger
+import androidx.camera.video.audio.wrapper.DefaultAudioRecordWrapper
 
 /**
  * An implementation of [ThinkletMic] using XFE feature.
  */
-internal object XfeThinkletMic : ThinkletMic {
+@OptIn(ExperimentalXfeFeature::class)
+internal class XfeThinkletMic(private val audioManager: AudioManager) : ThinkletMic {
+    private companion object {
+        const val TAG = "XfeThinkletMic"
+    }
 
     override fun getAudioSettingsPatcher(): ThinkletAudioSettingsPatcher = XfeAudioSettingsPatcher()
 
-    override fun getAudioRecordWrapperFactory(): ThinkletAudioRecordWrapperFactory? = null
+    @SuppressLint("MissingPermission", "RestrictedApi")
+    override fun getAudioRecordWrapperFactory(): ThinkletAudioRecordWrapperFactory? {
+        if (!audioManager.configureXfe(true)) {
+            Logger.e(TAG, "Failed to configureXfe")
+            return null
+        }
+
+        Logger.d(TAG, "getAudioRecordWrapperFactory")
+        return ThinkletAudioRecordWrapperFactory { audioSource, audioFormat, channelCount, sampleRate ->
+            // replace channelCount(1 or 2) to CHANNEL_IN_MONO or CHANNEL_IN_STEREO
+            val channelMask =
+                if (channelCount == 1) AudioFormat.CHANNEL_IN_MONO else AudioFormat.CHANNEL_IN_STEREO
+            // get minimum buffer size.
+            val bufSize = AudioRecord.getMinBufferSize(sampleRate, channelMask, audioFormat)
+            // create audioRecord.
+            val audioRecord = AudioRecord.Builder()
+                .setAudioSource(audioSource)
+                .setAudioFormat(
+                    AudioFormat.Builder()
+                        .setEncoding(audioFormat)
+                        .setSampleRate(sampleRate)
+                        .setChannelMask(channelMask)
+                        .build()
+                )
+                .setBufferSizeInBytes(bufSize)
+                .build()
+            object : DefaultAudioRecordWrapper(audioRecord) {
+                override fun release() {
+                    super.release()
+                    audioManager.configureXfe(false)
+                    Logger.d(TAG, "called configureXfe(false)")
+                }
+            }
+        }
+    }
 }
 
 /**
- * A mic mode for using a XFE feature.
+ * A mic mode for recording using XFE + 5ch mics.
  *
- * To use this mic mode, the caller must configure to enable the XFE in advance.
+ * The number of channels output is mono(1ch) only.
+ * @param audioManager AudioManager
  */
-val ThinkletMics.Xfe: ThinkletMic
-    get() = XfeThinkletMic
+fun ThinkletMics.Xfe(audioManager: AudioManager): ThinkletMic = XfeThinkletMic(audioManager)
