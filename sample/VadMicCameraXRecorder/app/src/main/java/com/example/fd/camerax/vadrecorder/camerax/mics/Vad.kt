e6cc5713f0678a23ba6870bd0f5341f664563ac3
Maehata Kenta <maehata@fairydevices.jp>
2024-11-14 15:13:36 +0900

    Add lock

--- sample/VadMicCameraXRecorder/app/src/main/java/com/example/fd/camerax/vadrecorder/camerax/mics/Vad.kt
+++ sample/VadMicCameraXRecorder/app/src/main/java/com/example/fd/camerax/vadrecorder/camerax/mics/Vad.kt
@@ -1,6 +1,7 @@
 package com.example.fd.camerax.vadrecorder.camerax.mics
 
 import android.content.Context
+import androidx.annotation.GuardedBy
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
@@ -12,6 +13,8 @@ import com.konovalov.vad.silero.config.Mode
 import com.konovalov.vad.silero.config.SampleRate
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.launch
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 /**
  * Voice Active Detection
@@ -22,31 +25,39 @@ internal class Vad(
     private val context: Context,
     private val lifecycleOwner: LifecycleOwner
 ) {
+    private val engineLock = ReentrantLock()
+
+    @GuardedBy("engineLock")
     private var engine: VadSilero? = null
 
     init {
         lifecycleOwner.lifecycleScope.launch {
             lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 Logging.d("create VAD")
-                engine = VadSilero(
-                    context,
-                    sampleRate = SampleRate.SAMPLE_RATE_16K,
-                    frameSize = FrameSize.FRAME_SIZE_1024,
-                    mode = Mode.NORMAL,
-                    silenceDurationMs = 300,
-                    speechDurationMs = 50
-                )
+                engine = engineLock.withLock {
+                    VadSilero(
+                        context,
+                        sampleRate = SampleRate.SAMPLE_RATE_16K,
+                        frameSize = FrameSize.FRAME_SIZE_1024,
+                        mode = Mode.NORMAL,
+                        silenceDurationMs = 300,
+                        speechDurationMs = 50
+                    )
+                }
                 try {
                     awaitCancellation()
                 } finally {
-                    engine?.close()
+                    engineLock.withLock {
+                        engine?.close()
+                        engine = null
+                    }
                     Logging.d("close VAD")
                 }
             }
         }
     }
 
-    fun isSpeech(byteArray: ByteArray): Boolean {
+    fun isSpeech(byteArray: ByteArray): Boolean = engineLock.withLock {
         return if (engine?.isSpeech(byteArray) == true) {
             Logging.d("isSpeech")
             true
